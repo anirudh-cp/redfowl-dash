c3e13966180112271f71cd21644176ad36ac8509
Anirudh C P <74400035+anirudh-cp@users.noreply.github.com>
2022-10-20 03:10:52 +0530

    PDF Generator Endpoint
    Depending on the resource and the UUID a PDF is generated.

--- backend/api/Views/MOM.py
+++ backend/api/Views/MOM.py
@@ -2,7 +2,6 @@ from api.models import minutes_of_meeting
 from api.serializers import MOMSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status, permissions
 
 import sys
