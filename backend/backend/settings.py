450329fe61318283268976c061d9fead354c39e9
Anirudh C P <74400035+anirudh-cp@users.noreply.github.com>
2022-10-24 00:36:27 +0530

    Changed env variable name to SECRET_KEY

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,7 +30,7 @@ dotenv.load_dotenv(ENV_FILE_PATH)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG')) == "1"
