898bdcd100a18d45450a31e5c416a00c894af0a9
Anirudh C P <74400035+anirudh-cp@users.noreply.github.com>
2022-10-24 01:10:08 +0530

    Title + Manifest Changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -140,6 +140,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+WEBPACK_LOADER = {
+    'MANIFEST_FILE': os.path.join(BASE_DIR, "frontend/build/manifest.json"),
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
